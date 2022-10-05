0802b42bf85b22c900f3e82a305696468bb3291f
Martín Gutiérrez <martin.gutierrez@oracle.com>
2022-10-05 15:38:01 -0300

    Normal

--- 4-Despliega-Weblogic-Kubernetes/1despliegaDominio.sh
+++ 4-Despliega-Weblogic-Kubernetes/1despliegaDominio.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-helm install domain01 domain --repo oci://$WLREPO/$WLREPODOM/helm/domain --version 1.0 --set image.repository=$WLREPOIMG,weblogic.managedServersCount=2,image.repo=$WLREPO,image.pullUsername=$WLREPODOM/$WLUSU,image.pullPassword=$WLTOKENCLA 
+# solo si esta helm version 3.8 o superior
+# helm install domain01 domain --repo oci://$WLREPO/$WLREPODOM/helm/domain --version 1 --set image.repository=$WLREPOIMG,weblogic.managedServersCount=2,image.repo=$WLREPO,image.pullUsername=$WLREPODOM/$WLUSU,image.pullPassword=$WLTOKENCLA 
+
+helm install domain01 weblogic-operator-oke/3-Crea-Imagen-Dominio-Weblogic/ejemplohelm --set image.repository=$WLREPOIMG,weblogic.managedServersCount=2,image.repo=$WLREPO,image.pullUsername=$WLREPODOM/$WLUSU,image.pullPassword=$WLTOKENCLA
