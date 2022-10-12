c69e5a4e1a5f6e09f691ba7e4ccb0147fe352a87
Martín Gutiérrez <martin.gutierrez@oracle.com>
2022-10-12 14:30:06 -0300

    Normal

--- 3-Crea-Imagen-Dominio-Weblogic/2generaChartHelm.sh
+++ 3-Crea-Imagen-Dominio-Weblogic/2generaChartHelm.sh
@@ -4,7 +4,11 @@
 # Opcional: Anter del primer push, crear repositorio en un compartment
 # Se debe consegir el ocid del compartment
 # oci artifacts container repository create --display-name "helm/domain" --compartment-id ocid
+echo ""
+echo "Chart dominio"
 helm package weblogic-operator-oke/3-Crea-Imagen-Dominio-Weblogic/ejemplohelm/domain
 helm push domain-0.1.0.tgz  oci://$YYREPO/$YYREPODOM/helm
+echo ""
+echo "Chart Ingress"
 helm package weblogic-operator-oke/3-Crea-Imagen-Dominio-Weblogic/ejemplohelm/ingress
 helm push ingress-0.1.0.tgz  oci://$YYREPO/$YYREPODOM/helm
